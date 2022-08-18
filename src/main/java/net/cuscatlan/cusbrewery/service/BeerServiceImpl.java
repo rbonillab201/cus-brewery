f3c54c6507a99d8270db2a5c5f3aa8f35f3111ea
rbonilla <renato_bonilla@dai.com>
2022-08-18 16:20:20 -0600

    Se ha incorporado el DeleteMapping

--- src/main/java/net/cuscatlan/cusbrewery/service/BeerServiceImpl.java
+++ src/main/java/net/cuscatlan/cusbrewery/service/BeerServiceImpl.java
@@ -2,8 +2,11 @@ package net.cuscatlan.cusbrewery.service;
 
 import java.util.UUID;
 import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
 import net.cuscatlan.cusbrewery.web.model.BeerDTO;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
@@ -29,6 +32,12 @@ public class BeerServiceImpl implements BeerService {
 	public void updateBeer(BeerDTO beerDTO, UUID beerId ) {
 		// TODO hay que completarlo, llegado el momento
 		
+	}
+
+	@Override
+	public void deleteBeer(UUID beerId) {
+		// TODO Auto-generated method stub
+		log.debug("Se ha borrado el registro " + beerId.toString());
 	}
 
 }
--- src/main/java/net/cuscatlan/cusbrewery/web/controller/BeerController.java
+++ src/main/java/net/cuscatlan/cusbrewery/web/controller/BeerController.java
@@ -4,12 +4,14 @@ import java.util.UUID;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import net.cuscatlan.cusbrewery.service.BeerService;
 import net.cuscatlan.cusbrewery.web.model.BeerDTO;
@@ -32,6 +34,7 @@ public class BeerController {
 		return new ResponseEntity<>(beerService.getBeerById(beerId),HttpStatus.OK);
 	}
 	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@PostMapping 
 	public ResponseEntity saveBeer(@RequestBody BeerDTO beerDTO) {
 		
@@ -41,12 +44,18 @@ public class BeerController {
 		return new ResponseEntity( "/api/v1/beer/" + headers,HttpStatus.CREATED);
 	}
 	
+	@SuppressWarnings("rawtypes")
 	@PutMapping("/{beerId}")
 	public ResponseEntity updateBeer(@RequestBody BeerDTO beerDTO, @PathVariable("beerId") UUID beerId) {
 		beerService.updateBeer(beerDTO,beerId);
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
 	}
 	
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{beerId}")
+	public void deleteBeer(@PathVariable("beerId") UUID beerId ) {
+		beerService.deleteBeer(beerId);
+	}
 	
 	
 	
